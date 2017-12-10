691dbb8f4fcce9b1de8825d82f4733beb5afd1bc
15057895240 <443003203@qq.com>
2017-12-10 22:03:14 +0800

    2017/12/10
    新增一楼

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bookuu7
+个人前端学习项目
+原版地址www.bookuu.com
--- js/index.js
+++ js/index.js
@@ -2,10 +2,26 @@
     $(".floor .toggle").on("click","a",function(){    
         //console.log($(this).html());
         let i=$(this).index();
+        let j=$(".toggle").index(this.parentNode);
         $(this).addClass("active");
         $(this).siblings().removeClass("active");
-        $(`.floor .content:eq(${i})`).addClass("index");
-        $(`.floor .content:not(:eq(${i}))`).removeClass("index");
+        $(`.floor:eq(${j}) .content:eq(${i})`).addClass("index");
+        $(`.floor:eq(${j}) .content:not(:eq(${i}))`).removeClass("index");
     });
-
+	$(".floor .content").on("mouseenter","div.model",function(){
+		//console.log(this);
+		$(this).css({
+			boxShadow :`0 0 1px 1px #666`,
+			zIndex:4
+		})
+		$(this).children(":first").css("transform","scale(1.1)")
+	})
+		$(".floor .content").on("mouseleave","div.model",function(){
+		//console.log(this);
+		$(this).css({
+			boxShadow :``,
+			zIndex:""
+		})
+		$(this).children(":first").css("transform",'')
+	})
 })()
